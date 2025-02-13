56ab88dca9deb10e9feaf747dcab7529ea323a29
Garimabhardwaj06 <garima.b366@gmail.com>
2025-02-12 23:21:24 -0500

    handle custom delimiter

--- incubytes_tdd.py
+++ incubytes_tdd.py
@@ -7,12 +7,28 @@ def add(inpStr: str) -> int:
         return sumValue
     
     delimiter = ","
-    inpStr = inpStr.replace('\n', delimiter)
-    inpStr = inpStr.split(delimiter)
+
+
+    if inpStr.startswith("//"):
+        delimiter_part, inpStr = inpStr.split('\n', 1)
+        custom_delimiters = re.findall(r'\[(.*?)\]', delimiter_part)
+        if custom_delimiters:
+            delimiter = '|'.join(map(re.escape, custom_delimiters))
+        else:
+            delimiter = re.escape(delimiter_part[2:])
     
-    sumValue = reduce(lambda x,y: x+y, map(lambda x: int(x),inpStr) )
+    inpStr = re.sub(r'\n', delimiter, inpStr)
+    number_list = re.split(delimiter, inpStr)
+    
+    numValues = list(map(lambda x: int(x), number_list) )
+
+
+    # inpStr = inpStr.replace('\n', delimiter)
+    # inpStr = inpStr.split(delimiter)
+    
+    sumValue = reduce(lambda x,y: x+y, map(lambda x: int(x),numValues) )
     return sumValue
 
 
-outValue = add("1\n2,3")
+outValue = add("//[***]\n1***2000***3")
 print(f'sum value is ------------>{outValue}')
