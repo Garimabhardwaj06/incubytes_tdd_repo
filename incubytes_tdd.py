445728e34bf07a4c19fb99ae493b45e60092391a
Garimabhardwaj06 <garima.b366@gmail.com>
2025-02-12 23:11:06 -0500

    Handling comma delimiter

--- incubytes_tdd.py
+++ incubytes_tdd.py
@@ -1,7 +1,15 @@
+from functools import reduce
+
 def add(inpStr: str) -> int:
     sumValue = 0
     if inpStr == '':
         return sumValue
+    
+    delimiter = ","
+    inpStr = inpStr.split(delimiter)
+    sumValue = reduce(lambda x,y: x+y, map(lambda x: int(x),inpStr) )
+    return sumValue
 
-outValue = add("")
+
+outValue = add("1")
 print(f'sum value is ------------>{outValue}')
