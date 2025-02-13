208b4a56bf315ca4ba1bde3aee9bb753262e67df
Garimabhardwaj06 <garima.b366@gmail.com>
2025-02-12 23:18:14 -0500

    Handle newline char

--- incubytes_tdd.py
+++ incubytes_tdd.py
@@ -1,4 +1,5 @@
 from functools import reduce
+import re
 
 def add(inpStr: str) -> int:
     sumValue = 0
@@ -6,10 +7,12 @@ def add(inpStr: str) -> int:
         return sumValue
     
     delimiter = ","
+    inpStr = inpStr.replace('\n', delimiter)
     inpStr = inpStr.split(delimiter)
+    
     sumValue = reduce(lambda x,y: x+y, map(lambda x: int(x),inpStr) )
     return sumValue
 
 
-outValue = add("1")
+outValue = add("1\n2,3")
 print(f'sum value is ------------>{outValue}')
